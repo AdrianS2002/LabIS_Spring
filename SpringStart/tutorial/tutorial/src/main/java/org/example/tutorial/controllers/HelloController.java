b853777c2fbb9daa8f5e5a5b848329c6d5ead881
AdrianS2002 <aschiop2002@yahoo.com>
2023-12-10 14:32:27 +0200

    Added dynamic templates

--- SpringStart/tutorial/tutorial/src/main/java/org/example/tutorial/controllers/HelloController.java
+++ SpringStart/tutorial/tutorial/src/main/java/org/example/tutorial/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.example.tutorial.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -33,21 +37,34 @@ public class HelloController {
 
     //Handles requests at path /hello?name=LaunchCode
     @RequestMapping(value="hello",method={RequestMethod.GET, RequestMethod.POST})
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello "+ name +" !";
+    public String helloWithQueryParam(@RequestParam String name, Model model){
+        String greeting= "Hello, " + name + "!";
+        model.addAttribute("greeting",greeting); //primul atribut trebuie sa fie la fel cu cel din html
+        return "hello";
     }
 
     //Handles request of the form /hello/LaunchCode
     //  /hello/hello
     @GetMapping("{name}")  //variabila din paranteza trebue sa fie aceeasi cu parametrul metodei
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello "+ name + " !";
+    public String helloWithPathParam(@PathVariable String name, Model model ){
+        String greeting= "Hello, " + name + "!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
     //hello/form
     @GetMapping("form")
     public String helloForm(){
       return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        names.add("Python");
+        model.addAttribute("names",names);
+        return "helloList";
     }
 }

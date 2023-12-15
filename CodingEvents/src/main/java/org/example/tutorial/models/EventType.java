aec521095123787e390740093d1cb6ef33211350
AdrianS2002 <aschiop2002@yahoo.com>
2023-12-15 13:51:15 +0200

    added functionalities from videos 16-26

---
+++ CodingEvents/src/main/java/org/example/tutorial/models/EventType.java
@@ -0,0 +1,18 @@
+package org.example.tutorial.models;
+
+public enum EventType {
+    CONFERENCE("Conference"),
+    MEETUP("Meetup"),
+    WORKSHOP("Workshop"),
+    SOCIAL("Social");
+
+    private final String displayName;
+
+    EventType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
